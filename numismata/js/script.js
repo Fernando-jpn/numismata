eeb0d08165cdf8ed7caad9693971c15eb0fe07c0
Fernando-jpn <fernandommrocha@outlook.com>
2024-12-04 23:30:13 -0300

    Add files via upload

--- numismata/js/script.js
+++ numismata/js/script.js
@@ -4,3 +4,29 @@ if(document.getElementById("redefinir").value){
     alert("disponivel na proxima atualização");
 }
 }
+
+function login(){
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if(usuario && senha){
+        alert("login efetuado!");
+    }
+    else {
+        alert("Por favor, insira seu login e senha.");
+    }
+}
+
+function cadastrar(){
+    var nome = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+    var senha = document.getElementById("senha").value;
+    var confirmarsenha = document.getElementById("confirmarsenha").value;
+
+    if(nome && email && senha && confirmarsenha){
+        alert("cadastro efetuado!");
+    }
+    else{
+        alert("Por favor, insira os campos em branco");
+    }
+}
